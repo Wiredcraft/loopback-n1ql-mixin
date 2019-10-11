008495cdee8d590ed627d6d13c38ff733f3413ab
pivstone <7299493+pivstone@users.noreply.github.com>
2019-10-11 14:27:55 +0800

    fix: keyword escape issue during indexing phase

--- test/fix.keyword.escape.test.js
+++ test/fix.keyword.escape.test.js
@@ -21,54 +21,63 @@ describe('keyword escape test', () => {
       }
     });
   app.loopback.modelBuilder.mixins.define('N1ql', N1ql);
-  const Book = Ds.createModel('Book', {
+  const User = Ds.createModel('User', {
     user: String, type: String,
     title: String, type: String,
     tags: Array, type: Array
-  }, { mixins: { 'N1ql': { primary: true } } });
-  Book.app = app;
+  }, {
+    indexes: {
+      user_idx: {
+        keys: {
+          user: 1
+        }
+      }
+    },
+    mixins: { 'N1ql': { primary: true } }
+  });
+  User.app = app;
   before('Prepare', async() => {
     await Ds.autoupdate();
-    await Book.create({ user: 'name', title: 'title', tags: ['sci-fi', 'war'] });
-    await Book.create({ user: 'name2', title: 'title2', tags: [{ name: 'sci-fi' }, { name: 'war' }] });
+    await User.create({ user: 'name', title: 'title', tags: ['sci-fi', 'war'] });
+    await User.create({ user: 'name2', title: 'title2', tags: [{ name: 'sci-fi' }, { name: 'war' }] });
     await wait(300); // Couchbase have a read delay. We have to waiting for couchbase updated index
   });
   after('Clear', async() => {
-    await Book.destroyAll();
+    await User.destroyAll();
   });
   describe('query method', () => {
     it('should support inq query', async() => {
-      const books = await Book.query({ where: { user: { inq: ['name', 'name2'] } }, order: ['user ASC'] });
-      expect(books.length).to.be.eql(2);
-      expect(books[0].user).to.be.eql('name');
+      const users = await User.query({ where: { user: { inq: ['name', 'name2'] } }, order: ['user ASC'] });
+      expect(users.length).to.be.eql(2);
+      expect(users[0].user).to.be.eql('name');
     });
 
     it('should return empty result', async() => {
-      const books = await Book.query({ where: { user: { inq: ['not_exists', 'not_exists2'] } }, order: ['user ASC'] });
-      expect(books.length).to.be.eql(0);
+      const users = await User.query({ where: { user: { inq: ['not_exists', 'not_exists2'] } }, order: ['user ASC'] });
+      expect(users.length).to.be.eql(0);
     });
 
     it('should return ONLY one result', async() => {
-      const books = await Book.query({ where: { user: { inq: ['not_exists', 'name'] } }, order: ['user ASC'] });
-      expect(books.length).to.be.eql(1);
-      expect(books[0].user).to.be.eql('name');
+      const users = await User.query({ where: { user: { inq: ['not_exists', 'name'] } }, order: ['user ASC'] });
+      expect(users.length).to.be.eql(1);
+      expect(users[0].user).to.be.eql('name');
     });
   });
 
   describe('count method', () => {
     it('should support inq query', async() => {
-      const books = await Book.sum({ where: { user: { inq: ['name', 'name2'] } } });
-      expect(books).to.be.eql(2);
+      const users = await User.sum({ where: { user: { inq: ['name', 'name2'] } } });
+      expect(users).to.be.eql(2);
     });
 
     it('should return ZERO', async() => {
-      const books = await Book.sum({ where: { user: { inq: ['not_exists', 'not_exists2'] } } });
-      expect(books).to.be.eql(0);
+      const users = await User.sum({ where: { user: { inq: ['not_exists', 'not_exists2'] } } });
+      expect(users).to.be.eql(0);
     });
 
     it('should return 1', async() => {
-      const books = await Book.sum({ where: { user: { inq: ['not_exists', 'name'] } } });
-      expect(books).to.be.eql(1);
+      const users = await User.sum({ where: { user: { inq: ['not_exists', 'name'] } } });
+      expect(users).to.be.eql(1);
     });
   });
 });
